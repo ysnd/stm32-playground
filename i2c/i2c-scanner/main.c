406f73ded0aed5bf1487fa1966ea367c262a31fe
ysnd <yusnadi.aunur@gmail.com>
2026-03-09 14:23:57 +0700

    feat: i2c address scanner with lcd display

---
+++ i2c/i2c-scanner/main.c
@@ -0,0 +1,184 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_def.h"
+#include "stm32f1xx_hal_i2c.h"
+
+#define LCD_ADDR (0x27 << 1)
+
+I2C_HandleTypeDef hi2c1;
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_I2C1_Init(void) {
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void lcd_send_cmd(char cmd) {
+    uint8_t data_u, data_l;
+    uint8_t data_t [4];
+
+    data_u = cmd & 0xF0;
+    data_l = (cmd << 4) & 0xF0;
+
+    data_t[0] = data_u | 0x0C;
+    data_t[1] = data_u | 0x08;
+    data_t[2] = data_l | 0x0C;
+    data_t[3] = data_l | 0x08;
+
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, data_t, 4, 100);
+}
+
+void lcd_send_data(char data) {
+    uint8_t data_u, data_l;
+    uint8_t data_t[4];
+
+    data_u = data & 0xF0;
+    data_l = (data << 4) & 0xF0;
+
+    data_t[0] = data_u | 0x0D;
+    data_t[1] = data_u | 0x09;
+    data_t[2] = data_l | 0x0D;
+    data_t[3] = data_l | 0x09;
+
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, data_t, 4, 100);
+}
+
+void lcd_clear(void) {
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_set_cursor(int row, int col) {
+    switch (row) {
+        case 0:
+            lcd_send_cmd(0x80 + col);
+            break;
+
+        case 1:
+            lcd_send_cmd(0xC0 + col);
+            break;
+    }
+}
+
+void lcd_print(char *str) {
+    while (*str) lcd_send_data(*str++);    
+}
+
+void lcd_clear_line(int line) {
+    lcd_set_cursor(line, 0);
+    for (int i=0; i<16; i++) {
+        lcd_send_data(' ');
+    }
+    lcd_set_cursor(line, 0);
+}
+
+void lcd_init(void) {
+    HAL_Delay(50);
+    
+    lcd_send_cmd(0x30);
+    HAL_Delay(5);
+    lcd_send_cmd(0x30);
+    HAL_Delay(1);
+    lcd_send_cmd(0x30);
+
+    lcd_send_cmd(0x20);
+
+    lcd_send_cmd(0x28);
+    lcd_send_cmd(0x08);
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+    lcd_send_cmd(0x06);
+    lcd_send_cmd(0x0C);
+}
+ 
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    
+    MX_I2C1_Init();
+
+    lcd_init();
+
+    while (1)
+    {
+        char buffer[16];
+
+        lcd_clear();
+        lcd_set_cursor(0, 0);
+        lcd_print("Scanning........");
+        HAL_Delay(1000);
+
+        for (uint8_t addr = 1; addr < 128; addr++) {
+            if (HAL_I2C_IsDeviceReady(&hi2c1, addr<<1, 1, 10) == HAL_OK) {
+                lcd_clear();
+                lcd_set_cursor(0, 0);
+                lcd_print("Device Found");
+                sprintf(buffer, "Address=0x%X",addr);
+                lcd_set_cursor(1, 0);
+                lcd_print(buffer);
+
+                HAL_Delay(2000);
+            }
+        }
+        lcd_clear();
+        lcd_set_cursor(0,0);
+        lcd_print("Scan Done");
+    }
+}
+
+
+
