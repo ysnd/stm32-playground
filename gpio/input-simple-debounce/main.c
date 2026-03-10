6a71ddeb2239a15faeff8eba172063013cb57b40
ysnd <yusnadi.aunur@gmail.com>
2026-03-10 14:22:10 +0700

    update input simple debouncing with lcd debugging

--- gpio/input-simple-debounce/main.c
+++ gpio/input-simple-debounce/main.c
@@ -1,6 +1,10 @@
 #include "stm32f1xx_hal.h"
 #include <stdint.h>
+
 #define DEBOUNCE_MS 20
+#define LCD_ADDR (0x27 << 1)
+
+I2C_HandleTypeDef hi2c1;
 
 uint32_t t_start = 0;
 uint8_t stable = 1;
@@ -44,16 +48,114 @@ void SystemClock_Config(void)
   }
 }
 
+static void MX_I2C1_Init(void) {
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void lcd_send_cmd(char cmd) {
+    uint8_t data_u, data_l;
+    uint8_t data_t [4];
+
+    data_u = cmd & 0xF0;
+    data_l = (cmd << 4) & 0xF0;
+
+    data_t[0] = data_u | 0x0C;
+    data_t[1] = data_u | 0x08;
+    data_t[2] = data_l | 0x0C;
+    data_t[3] = data_l | 0x08;
+
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, data_t, 4, 100);
+}
+
+void lcd_send_data(char data) {
+    uint8_t data_u, data_l;
+    uint8_t data_t[4];
+
+    data_u = data & 0xF0;
+    data_l = (data << 4) & 0xF0;
+
+    data_t[0] = data_u | 0x0D;
+    data_t[1] = data_u | 0x09;
+    data_t[2] = data_l | 0x0D;
+    data_t[3] = data_l | 0x09;
+
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, data_t, 4, 100);
+}
+
+void lcd_clear(void) {
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_set_cursor(int row, int col) {
+    switch (row) {
+        case 0:
+            lcd_send_cmd(0x80 + col);
+            break;
+
+        case 1:
+            lcd_send_cmd(0xC0 + col);
+            break;
+    }
+}
+
+void lcd_print(char *str) {
+    while (*str) lcd_send_data(*str++);    
+}
+
+void lcd_clear_line(int line) {
+    lcd_set_cursor(line, 0);
+    for (int i=0; i<16; i++) {
+        lcd_send_data(' ');
+    }
+    lcd_set_cursor(line, 0);
+}
+
+void lcd_init(void) {
+    HAL_Delay(50);
+    
+    lcd_send_cmd(0x30);
+    HAL_Delay(5);
+    lcd_send_cmd(0x30);
+    HAL_Delay(1);
+    lcd_send_cmd(0x30);
+
+    lcd_send_cmd(0x20);
+
+    lcd_send_cmd(0x28);
+    lcd_send_cmd(0x08);
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+    lcd_send_cmd(0x06);
+    lcd_send_cmd(0x0C);
+}
+
 
 int main(void)
 {
     HAL_Init();
     SystemClock_Config();
     
+    
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    MX_I2C1_Init();
+    lcd_init();
 
     GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
@@ -82,8 +184,12 @@ int main(void)
 
         if (stable == 0) {
 		    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 1);
+            lcd_set_cursor(0, 0);
+            lcd_print("LED ON");
 		} else{
 		    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 0);
+            lcd_set_cursor(0, 0);
+            lcd_print("LED OFF");
         }
     }
 }
